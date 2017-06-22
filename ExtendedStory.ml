c26ac94f206779f787be2cbedf9e2dba7beae67c
Mickaël LAURENT <esh4rk@gmail.com>
2017-06-22 18:07:28 +0200

    update

--- ExtendedStory.ml
+++ ExtendedStory.ml
@@ -7,7 +7,15 @@ let main () =
   (* Choose intervention (heuristic) *)
   let interventions = heuristic_choose_interventions () in
   (* Compute and sample counterfactuals traces *)
+  (* If the htreshol is excedeed : *)
   (* Find the most blocked event in the causal core *)
+  (* Find the last event that inhibits it in each counterfactual trace *)
+  (* Select one of them (heuristic?) and compute its causal core *)
+  (* Merge the two causal cores *)
+  (* For each events of the contrefactual story that is caused directly by an event present in the factual story (including init) :
+  Find the last event in the factual trace (among those that we blocked) that prevent it
+  (it should have happenned between the factual and contrefactual events), compute its story and merge.
+  Put an inhibition arrow between these two events. *)
   ()
 
 let () = main ()
