d105e9370756e4990a535cf44836a9f1bf61fa56
Mickaël Laurent <E-Sh4rk@users.noreply.github.com>
2017-06-22 22:21:16 -0400

    Update compute.ml

--- compute.ml
+++ compute.ml
@@ -5,7 +5,7 @@ let heuristic_choose_interventions () : interventions = []
 
 let compute_extended_story model trace rule_name =
   (* Compute factual causal core *)
-  (* Convert a copy of this causal core to an extended story *)
+  (* Initialize a list of counterfactual extended causal cores *)
   
   (* Choose intervention (heuristic) depending on the trace and the current extended story :
   For example :
@@ -17,10 +17,13 @@ let compute_extended_story model trace rule_name =
   (* Take one of the counterfactual traces (heuristic? random?) *)
   (* Find the last events that has inhibited the event of the causal core that has been blocked :
   it is the last events that changed the value of a tested logical site from a good value to a wrong value. *)
-  (* Select the first of these events and compute its causal core *)
-  (* Merge this contrefactual core with the extended story, add the corresponding inhibition arrow, and add missing precendence relations between them *)
-  (* For each events of the contrefactual core that is caused directly by an event present in the factual core (including init) :
-  Find the last events in the factual trace among those that we blocked that prevent it (same method as above)
-  (it should have happenned between the factual and contrefactual events), compute their causal cores,
-  merge them and add corresponding inhibition arrows. *)
+  (* Select the first (earliest) of these events and compute its causal core. Add this counterfactual causal core to the list and indicate where go the inhibition arrow. *)
+  (* For each events of this counterfactual causal core <that has no counterfactual-only cause|that as at least one factual cause>,
+  find the last events in the factual trace among those that we blocked that prevent it (same method as above). Indicate in the counterfactual core the origin of this inhibition arrow. *)
+  (* Compute a new factual causal core with all the previous events + these inhibitive events <+ other factual events of the counterfactual core if we want to have more links with the factual core at the end>. *)
+  
+  (* At the end, merge the factual causal core and all the counterfactual causal cores. Depending on the details wanted by the user, we can :
+     - Merge everything by merging together nodes that represent the same event
+     - Keep only counterfactual-only events of counterfactual cores
+  *)
   ()
