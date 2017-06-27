c3ca9339fb84ef0fba887a17f5eda48d3e121c9e
Mickaël LAURENT <esh4rk@gmail.com>
2017-06-27 21:04:32 +0200

    update

--- compute.ml
+++ compute.ml
@@ -27,6 +27,17 @@ let trace_succeed eoi_id trace = match Array.length trace with
     | _ -> false
   end
 
+let resimulate_and_sample nb eoi_id model block_pred stop_pred trace =
+  let rec aux nb acc = match nb with
+    | 0 -> acc
+    | n -> let (nb_failed,wit) = acc in 
+    let ctrace = Array.of_list (resimulate model block_pred stop_pred trace) in
+    if trace_succeed eoi_id ctrace then
+    aux (nb-1) (nb_failed,wit)
+    else
+    aux (nb-1) (nb_failed+1,Some ctrace)
+  in aux nb (0,None)
+
 let compute_extended_story model trace rule_name config =
   (* Determining event of interest *)
   let eoi_id = get_first_rule_event model rule_name trace in
@@ -40,22 +51,30 @@ let compute_extended_story model trace rule_name config =
   let interventions = heuristic_choose_interventions () in
   let block_pred = interventions_to_predicate interventions
   and stop_pred = stop_conditions_to_predicate [Event_has_happened eoi_id;Event_has_not_happened eoi_id] in
-  (* Compute and sample counterfactuals traces (resimulation stops when eid has happened/has been blocked) *)
-  let samples = Array.make config.nb_samples trace in
-  let samples = Array.map (fun t -> Array.of_list (resimulate model block_pred stop_pred t)) samples in
-  let failed_samples = List.filter (fun t -> not (trace_succeed eoi_id t)) (Array.to_list samples) in
-  (* If the threshold is excedeed : *)
-  (* Take one of the counterfactual traces (heuristic? random among the traces that block the eoi? smallest core? more blocked event?) *)
-  (* Find the last events that has inhibited the first event of the causal core that has been blocked :
-  it is the last events that changed the value of a tested logical site from a good value to a wrong value. *)
-  (* Select the first (earliest) of these events and compute its causal core. Add this counterfactual causal core to the list and indicate where go the inhibition arrow. *)
-  (* For each events of this counterfactual causal core <that has no counterfactual-only cause|that as at least one factual cause>,
-  find the last events in the factual trace among those that we blocked that prevent it (same method as above). Indicate in the counterfactual core the origin of these inhibition arrows. *)
-  (* Update the factual core : compute a new factual causal core with all the previous added events + events with an inhibitive arrow <+ other factual events of the counterfactual core if we want to have more links with the factual core at the end>. *)
-  
-  (* At the end, merge the factual causal core and all the counterfactual causal cores. Depending on the details wanted by the user, we can :
-     - Merge everything by merging together nodes that represent the same event
-     - Keep only counterfactual-only events of counterfactual cores
-     Don't forget to put all the inhibition arrows that had been found.
-  *)
-  ()
+  (* Compute and sample counterfactual traces (resimulation stops when eid has happened/has been blocked) *)
+  (* Take one of the counterfactual traces that failed as witness
+  (heuristic? random among the traces that block the eoi? smallest core? more blocked event?) *)
+  let (nb_failed,ctrace) = resimulate_and_sample config.nb_samples eoi_id model block_pred stop_pred trace in
+  let ratio = 1.0 -. (float_of_int nb_failed)/.(float_of_int config.nb_samples) in
+  if ratio >= config.threshold then
+  (
+
+  )
+  else
+  (
+    let Some ctrace = ctrace in
+    ()
+    (* If we are below the threshold : *)
+    (* Find the last events that has inhibited the first event of the causal core that has been blocked :
+    it is the last events that changed the value of a tested logical site from a good value to a wrong value. *)
+    (* Select the first (earliest) of these events and compute its causal core. Add this counterfactual causal core to the list and indicate where go the inhibition arrow. *)
+    (* For each events of this counterfactual causal core <that has no counterfactual-only cause|that as at least one factual cause>,
+    find the last events in the factual trace among those that we blocked that prevent it (same method as above). Indicate in the counterfactual core the origin of these inhibition arrows. *)
+    (* Update the factual core : compute a new factual causal core with all the previous added events + events with an inhibitive arrow <+ other factual events of the counterfactual core if we want to have more links with the factual core at the end>. *)
+    
+    (* At the end, merge the factual causal core and all the counterfactual causal cores. Depending on the details wanted by the user, we can :
+      - Merge everything by merging together nodes that represent the same event
+      - Keep only counterfactual-only events of counterfactual cores
+      Don't forget to put all the inhibition arrows that had been found.
+    *)
+  )
