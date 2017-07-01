b747a0fa0ff0a6fe628b62425dcf8154643857d5
Mickaël LAURENT <esh4rk@gmail.com>
2017-07-01 17:52:06 +0200

    update

--- heuristics.ml
+++ heuristics.ml
@@ -4,4 +4,6 @@ open Interface
  (* For example :
     - Block permanently in trace T every event that involve agents in the factual core and that are not in the factual causal core.
     - Block permanently an event that is suspected to have an impact later.*)
-let heuristic_choose_interventions trace core : interventions = [] (* TODO *)
+let heuristic_choose_interventions trace core : interventions =
+  let agents_involved_in_core = [] in
+  [] (* TODO *)
