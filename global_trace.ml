5fd0f446090e74fad6e13aeb2cf47bc4167dc748
Mickaël LAURENT <esh4rk@gmail.com>
2017-07-05 20:39:29 +0200

    struct global trace

--- global_trace.ml
+++ global_trace.ml
@@ -1,6 +1,7 @@
 open Ext_tools
 
 (* Abstract types *)
+
 type global_info = (int*int) array (* Associate an index with (id,order) *)
 type trace_info = Trace_explorer.t * Causal_core.var_info_table
 type global_trace = trace_info * global_info
@@ -9,7 +10,8 @@ type global_info_builder = ( (int*int) list ) * int (* (list,next_rt_index,next_
 type global_trace_builder = (Trace.step list) * global_info_builder
 
 (* Not exported *)
-let set_id (_,gi) index id =
+
+let set_global_id (_,gi) index id =
   let (_,order) = gi.(index) in
   gi.(index) <- (id,order)
 
@@ -17,10 +19,50 @@ let set_order (_,gi) index order =
   let (id,_) = gi.(index) in
   gi.(index) <- (id,order)
 
+let nto_binary_search arr cmp value =
+  let rec nearest_element i min max dir =
+    match i+dir with
+    | i when i > max || i < min -> None
+    | i when cmp value arr.(i) = None -> nearest_element i min max dir
+    | i when cmp value arr.(i) = Some dir || cmp value arr.(i) = Some 0 -> Some i
+    | _ -> None
+  in
+  let rec aux i1 i2 = match i1, i2 with
+  | i1, i2 when i1 > i2 -> None
+  | i1, i2 -> begin match (i1+i2)/2 with
+    | i when cmp arr.(i) value = None ->
+    let ni = nearest_element i i1 i2 (-1) in
+    let ni = if ni = None then nearest_element i i1 i2 1 else ni in
+    begin match ni with
+      | None -> None
+      | Some ni when ni > i -> aux ni i2
+      | Some ni -> aux i1 ni
+    end
+    | i when cmp arr.(i) value = Some 0 -> Some i
+    | i when cmp arr.(i) value = Some (-1) -> aux (i+1) i2
+    | i when cmp arr.(i) value = Some 1 -> aux i1 (i-1)
+    | _ -> assert false
+  end in
+  aux 0 ((Array.length arr)-1)
+
+let unidir_binary_search arr first pred =
+  let rec aux i1 i2 = match i1, i2 with
+    | i1, i2 when i1 > i2 -> None
+    | _, i2 when first && not (pred arr.(i2)) -> None
+    | i1, _ when not first && not (pred arr.(i1)) -> None
+    | i1, _ when first && pred arr.(i1) -> Some i1
+    | _, i2 when not first && pred arr.(i2) -> Some i2
+    | i1, i2 -> begin match (i1+i2)/2 with
+      | i when pred arr.(i) -> if first then aux i1 i else aux i i2
+      | i -> if first then aux (i+1) i2 else aux i1 (i-1)
+    end
+  in
+  aux 0 ((Array.length arr)-1)
+
 (* Exported *)
 
 (* Basic functions *)
-let get_id (_,gi) index =
+let get_global_id (_,gi) index =
   let (id,_) = gi.(index) in
   id
 
@@ -40,7 +82,7 @@ let new_reference_trace te =
   let gi = Array.make ((Trace_explorer.last_step_id te) + 1) (0,0) in
   let gtr = ((te,vi),gi) in
   for i=0 to (Array.length gi) - 1 do
-    set_id gtr i i ;
+    set_global_id gtr i i ;
     set_order gtr i i
   done ; gtr
 
@@ -56,7 +98,7 @@ let add_counterfactual_step rtr (csteps,(cgi,ri,o)) cs =
   set_order rtr ri o ; (csteps,(cgi,ri+1,o+1))
   | Resimulation.Factual_happened s ->
   set_order rtr ri o ;
-  (s::csteps,((get_id rtr ri,o)::cgi,ri+1,o+1))
+  (s::csteps,((get_global_id rtr ri,o)::cgi,ri+1,o+1))
 
 let finalize_counterfactual_trace rtr (ctr,(cgi,ri,o)) =
   for i=ri to (length rtr) - 1 do
@@ -67,6 +109,26 @@ let finalize_counterfactual_trace rtr (ctr,(cgi,ri,o)) =
   let vi = Causal_core.init_var_infos te in
   ((te,vi), Array.of_list (List.rev cgi))
 
-(* Search functions : search ID, search order, search time... *)
+(* Search functions : search ID, search order... *)
+let search_global_id (_,gi) id =
+  let cmp (id1,_) (id2,_) = match id1, id2 with
+  | id1, id2 when id1 >= 0 && id2 < 0 -> None
+  | id1, id2 when id2 >= 0 && id1 < 0 -> None
+  | id1, id2 when id1 >= 0 && id2 >= 0 -> Some (Pervasives.compare id1 id2)
+  | id1, id2 -> Some (- (Pervasives.compare id1 id2))
+in nto_binary_search gi cmp (id,0)
 
-(* Trace functions : get_tests / get_actions, get_var_infos *)
+let search_first_after_order (_,gi) order =
+  let pred (_,o) = o > order in
+  unidir_binary_search gi true pred
+
+let search_last_before_order (_,gi) order =
+  let pred (_,o) = o < order in
+  unidir_binary_search gi false pred
+
+(* Trace analysis functions *)
+let get_tests tr index = Trace_explorer.Grid.tests index (get_trace_explorer tr)
+
+let get_actions tr index = Trace_explorer.Grid.actions index (get_trace_explorer tr)
+
+let get_history ((_,vi),_) var = Causal_core.get_modifications_history var vi
--- global_trace.mli
+++ global_trace.mli
@@ -2,7 +2,7 @@
 type global_trace
 type global_trace_builder
 
-val get_id : global_trace -> int -> int
+val get_global_id : global_trace -> int -> int
 val get_order : global_trace -> int -> int
 val get_step : global_trace -> int -> Trace.step
 val length : global_trace -> int
@@ -11,3 +11,11 @@ val new_reference_trace : Trace_explorer.t -> global_trace
 val new_counterfactual_trace_builder : unit -> global_trace_builder
 val add_counterfactual_step : global_trace -> global_trace_builder -> Resimulation.step -> global_trace_builder
 val finalize_counterfactual_trace : global_trace -> global_trace_builder -> global_trace
+
+val search_global_id : global_trace -> int -> int option (* Linear operation, logarithmic in general *)
+val search_first_after_order : global_trace -> int -> int option (* Logarithmic operation *)
+val search_last_before_order : global_trace -> int -> int option (* Logarithmic operation *)
+
+val get_tests : global_trace -> int -> Grid.constr list
+val get_actions : global_trace -> int -> Grid.constr list
+val get_history : global_trace  -> 'a Grid.var -> History.t
