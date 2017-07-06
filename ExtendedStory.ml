5a7ec4a8c32be5c943e291ba532857b6a1b23ccf
Mickaël LAURENT <esh4rk@gmail.com>
2017-07-06 23:48:21 +0200

    ext story printer

--- ExtendedStory.ml
+++ ExtendedStory.ml
@@ -5,12 +5,15 @@ open Ext_story_printer
 let file = ref ""
 let output_prefix = ref ""
 let rule_of_interest = ref ""
+let verbose = ref false
 
 let options = [
   ("-o", Arg.Set_string output_prefix,
    "prefix for the output file");
   ("-r", Arg.Set_string rule_of_interest,
-   "rule of interest")
+   "rule of interest");
+  ("--verbose", Arg.Set verbose,
+   "print annotated dot files");
 ]
 let description = ""
 
@@ -57,17 +60,11 @@ let main () =
       let eoi = get_first_eoi_after te !rule_of_interest 0 in
       let es = compute_extended_story te eoi config in
 
-      let options =
-      { ranksep            = 1.0 ;
-        show_strong_deps   = true ;
-        strong_deps_labels = true ;
-        dump_grid          = true ;
-        show_event_ids     = true ;
-        font               = "CMU Serif" ;
-      } in
-
       let oc = open_out (!output_prefix^".dot") in
       let fmt = Format.formatter_of_out_channel oc in
+      let options = 
+            if !verbose then Story_printer.def_options_detailed
+            else Story_printer.def_options_simple in
       print_extended_story es Hiding_factual_events options fmt ;
       close_out oc
     ) with Not_found -> logs "No event of interest found !"
--- ext_story_printer.ml
+++ ext_story_printer.ml
@@ -1,14 +1,4 @@
-open Story_printer
 open Ext_tools
-
-type print_options =
-  { ranksep            : float  ;
-    show_strong_deps   : bool   ;
-    strong_deps_labels : bool   ;
-    dump_grid          : bool   ;
-    show_event_ids     : bool   ;
-    font               : string ;
-  }
 
 type merging_mode = Hiding_factual_events | Merging_factual_events | No_merging
 
@@ -18,31 +8,46 @@ type merging_mode = Hiding_factual_events | Merging_factual_events | No_merging
     - Merge common factual events BUT if they have different causes, show all these relations with different colors
 *)
 
-let compute_precedence model subtrace =
+let max_time te = get_time_of_step (Trace_explorer.step (Trace_explorer.last_step_id te) te) 0.0
+
+let choose_color options tr i dummy =
+    match options.Story_printer.nodes_coloring with
+    | Build_order -> failwith "Build order coloring not implemented."
+    | Time ->
+        let maxT = max_time (Global_trace.get_trace_explorer tr) in
+        let t = get_time_of_step (Global_trace.get_step tr i) 0.0 in
+        let (r,v,b) = (0., 0., (1.0 -. 0.4 *. t /. maxT)) in
+        if Global_trace.get_global_id tr i < 0 then (b,v,r) else (r,v,b)
+
+let compute_precedence subtrace =
     let core = n_first_intergers (Global_trace.length subtrace) in
     let precedences = Precedence.transitive_reduction (Precedence.compute_precedence (Global_trace.get_trace_explorer subtrace) core) in
     List.map (fun (i1,i2) -> (Global_trace.get_global_id subtrace i1,Global_trace.get_global_id subtrace i2)) precedences
 
-let compute_activation model subtrace =
+let compute_activation subtrace =
     let core = n_first_intergers (Global_trace.length subtrace) in
     let activations = Precedence.compute_strong_deps (Global_trace.get_trace_explorer subtrace) core in
     List.map (fun (i1,c,i2) -> (Global_trace.get_global_id subtrace i1,c,Global_trace.get_global_id subtrace i2)) activations
 
-let print_factual_event already_printed fmt id step = ()
+let print_factual_event trace index already_printed options fmt =
+    let id = Global_trace.get_global_id trace index in
+    if not (IntSet.mem id already_printed) then
+    Story_printer.print_event options (Global_trace.get_trace_explorer trace) (choose_color options trace) fmt id (index,index)
+
 let print_precedence fmt (id1,id2) = ()
 let print_activation fmt (id1,c,id2) = ()
 
-let print_factual_part model fact print_act fmt =
+let print_factual_part fact options fmt =
     let pr x = Format.fprintf fmt x in
     for i=0 to (Global_trace.length fact)-1 do
-        print_factual_event [] fmt (Global_trace.get_global_id fact i) (Global_trace.get_step fact i)
+        print_factual_event fact i IntSet.empty options fmt
     done ;
-    let prec = compute_precedence model fact in
+    let prec = compute_precedence fact in
     List.iter (print_precedence fmt) prec ;
     pr "@;" ;
-    if print_act then
+    if options.Story_printer.show_strong_deps then
     (
-        let act = compute_activation model fact in
+        let act = compute_activation fact in
         List.iter (print_activation fmt) act ;
         pr "@;"
     ) ;
@@ -51,14 +56,13 @@ let print_factual_part model fact print_act fmt =
 
 let print_extended_story (fact,cps) mode options fmt =
     let pr x = Format.fprintf fmt x in
-    let model = Global_trace.get_model fact in
 
     pr "@[<v 2>digraph G{@;" ;
     pr "rankdir=\"TB\";@;" ;
-    pr "ranksep=%.2f;@;" options.ranksep ;
-    pr "node [fontname=\"%s\"];@;" options.font ;
-    pr "edge [fontname=\"%s\"];@;" options.font ;
+    pr "ranksep=%.2f;@;" options.Story_printer.ranksep ;
+    pr "node [fontname=\"%s\"];@;" options.Story_printer.font ;
+    pr "edge [fontname=\"%s\"];@;" options.Story_printer.font ;
     pr "@;" ;
 
-    let factual_already_printed = print_factual_part model fact options.show_strong_deps fmt in
+    let factual_already_printed = print_factual_part fact options fmt in
     (* TODO *) ()
